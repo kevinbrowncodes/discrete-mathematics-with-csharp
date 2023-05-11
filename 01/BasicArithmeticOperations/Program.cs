c6fad13d3ed2e68c800f76285807f07d00e9068f
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-05-11 13:54:20 -0400

    01 Perform all Basic Arithmetic Operations

---
+++ 01/BasicArithmeticOperations/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace BasicArithmeticOperations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result;
+            char operation;
+
+            Console.WriteLine("\nEnter the first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter the second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter an operation: ");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Addition: +");
+            Console.WriteLine("Subtraction: -");
+            Console.WriteLine("Multiplication: *");
+            Console.WriteLine("Division: /");
+            Console.WriteLine("--------------------");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine($"\n{num1} + {num2} = {result}");
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine($"\n{num1} - {num2} = {result}");
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine($"\n{num1} * {num2} = {result}");
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    Console.WriteLine($"\n{num1} / {num2} = {result}");
+                    break;
+                default:
+                    Console.WriteLine("\nInvalid Operation!");
+                    break;
+            }
+        }
+    }
+}
