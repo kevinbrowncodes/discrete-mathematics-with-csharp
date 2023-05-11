bef4432edfc2ab0587964f795824cd72289a58f6
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-05-11 16:00:27 -0400

    02 Generate the Factorial of Given Number

---
+++ 02/GenerateFactorial/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenerateFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num, factorial;
+
+            Console.WriteLine("Enter a number: ");
+            num = Convert.ToInt16(Console.ReadLine());
+
+            factorial = num;
+
+            for (i = num - 1; i >= 1; i--) 
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine($"\n{num}! = {factorial}");
+        }
+    }
+}
